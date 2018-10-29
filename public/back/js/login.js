1cde48ad4e3fb69e8dfd9dcee1143554dea5a945
zhen920907 <1192695660@qq.com>
2018-10-29 18:44:17 +0800

    登录功能页面完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,115 +1,127 @@
-/**
- * Created by Jepson on 2018/4/6.
- */
 
 // 防止全局变量污染, 等待dom渲染再执行
 $(function() {
+/*
+   * 1. 进行表单校验配置
+   *    校验要求:
+   *        (1) 用户名不能为空, 长度为2-6位
+   *        (2) 密码不能为空, 长度为6-12位
+   * */
 
-  // 1. 进行表单校验
-  //    校验要求: (1) 用户名不能为空
-  //              (2) 密码不能为空, 且必须是 6-12 位
-  $("#form").bootstrapValidator({
 
-    // 配置图标
-    feedbackIcons: {
-      valid: 'glyphicon glyphicon-ok',
-      invalid: 'glyphicon glyphicon-remove',
-      validating: 'glyphicon glyphicon-refresh'
+   //使用表单校验插件
+$('#form').bootstrapValidator({  
+
+  //1. 指定校验时的图标显示，默认是bootstrap风格
+  feedbackIcons: {
+    valid: 'glyphicon glyphicon-ok',  //校验成功的图标
+    invalid: 'glyphicon glyphicon-remove',  //校验失败的图标
+    validating: 'glyphicon glyphicon-refresh' //校验中的图标
+  },
+
+  //2. 指定校验字段,要先给input设置name属性
+  fields: {
+    //配置用户名校验，
+    username: {
+      // 配置校验规则
+      validators: {
+        //非空校验
+        notEmpty: {
+          // 非空提示
+          message: '用户名不能为空'
+        },
+
+        //长度校验
+        stringLength: {
+          min: 2,
+          max: 6,
+          message: '用户名长度必须在2到6之间'
+        },
+
+         // 配置ajax回调的提示
+         callback: {
+          message: '用户名不能为空'
+        }
+        
+      }
     },
 
-    // 对字段进行校验
-    fields: {
-      username: {
-        // 校验的规则
-        validators: {
-          // 非空校验
-          notEmpty: {
-            // 为空时显示的提示信息
-            message: "用户名不能为空"
-          },
-          // 长度要求 2-6 位
-          stringLength: {
-            min: 2,
-            max: 6,
-            message: "用户名长度必须是 2-6 位"
-          },
-          callback: {
-            message: "用户名不存在"
-          }
+
+     //配置密码校验，
+     password: {
+      //  配置校验规则
+      validators: {
+        //非空校验
+        notEmpty: {
+          // 非空提示
+          message: '密码不能为空'
+        },
+
+        //长度校验
+        stringLength: {
+          min: 6,
+          max: 12,
+          message: '密码长度必须在6到12之间'
+        },
+
+        // 配置ajax回调的提示
+        callback: {
+          message: '密码错误'
         }
-      },
-      password: {
-        validators: {
-          notEmpty: {
-            message: "密码不能为空"
-          },
-          // 长度校验
-          stringLength: {
-            min: 6,
-            max: 12,
-            message: "密码长度必须是6-12位"
-          },
-          // 专门用于配置回调提示信息的校验规则
-          callback: {
-            message: "密码错误"
-          }
-        }
+        
       }
-    }
-  });
+    },
+  }
+});
 
 
-  // 2. 进行登录请求
-  //    通过 ajax 进行登录请求
+// 2.登录成功的功能
+// 当表单校验成功时，会触发success.form.bv事件，此时会提交表单，这时候，通常我们需要禁止表单的自动提交，使用ajax进行表单的提交。
 
-  // 表单校验插件有一个特点, 在表单提交的时候进行校验
-  // 如果校验成功, 继续提交, 需要阻止这次默认的提交, 通过 ajax 进行请求提交
-  // 如果校验失败, 默认会阻止提交
-  $('#form').on("success.form.bv", function( e ) {
-    // 阻止默认的表单提交
-    e.preventDefault();
+$("#form").on('success.form.bv', function (e) {
+  e.preventDefault();
+  //使用ajax提交逻辑
 
-    //console.log($('#form').serialize());
+  $.ajax({
+    type: 'post',
+    url: '/employee/employeeLogin',
+    data: $('#form').serialize(),
+    dataType: 'json',
+    success: function(info){
+          console.log(info);
+          // 如果登录成功，跳转到首页
+          if(info.success){
+            location.href = 'index.html';
+          }
 
-    // 通过 ajax 进行登录请求
-    $.ajax({
-      type: "post",
-      url: "/employee/employeeLogin",
-      dataType: "json",
-      data: $('#form').serialize(),
-      success: function( info ) {
-        console.log( info )
-
-        if ( info.success ) {
-          // alert( "登录成功" );
-          location.href = "index.html";
-        }
-
-        if ( info.error === 1000 ) {
-          // alert( "用户名不存在" )
-          $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
-        }
-
-        if ( info.error === 1001 ) {
-          // alert( "密码错误" );
+          // 如果登录失败，错误是1000的话，就是校验用户名 校验失败的回调
+          if(info.error === 1000) {
+            // 提示用户名不存在
+          // alert( info.message );
+          // 调用插件实例方法, 更新校验状态成失败
           // updateStatus
-          // 参数1: 字段名称
-          // 参数2: 校验状态
-          // 参数3: 校验规则, 可以设置提示文本
-          $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback")
-        }
-      }
+          // 参数1: 校验字段
+          // 参数2: 校验状态  NOT_VALIDATED未校验, VALIDATING校验中, INVALID校验失败 or VALID成功
+          // 参数3: 配置校验规则, 用于配置提示信息
 
-    })
-
-  });
+          $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
+          }
 
 
-  // 3. 重置功能实现
-  $('[type="reset"]').click(function() {
-    console.log( 1111 );
-    // 除了重置文本, 还要重置校验状态
-    $('#form').data("bootstrapValidator").resetForm();
-  });
+          // 如果登录失败，错误是1001的话，就是验证密码校验失败的回调
+          if(info.error === 1001) {
+            $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+          }
+    },    
+  })
+});
 
+
+// 3.表单重置功能
+
+// 点击重置按钮，然后重置表单
+$(' [type="reset"]').on('click',function(){
+  $('#form').data('bootstrapValidator').resetForm(true);
+
+});
 });
